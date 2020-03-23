38449c3a592cf0a77f1c260ae097589f741c7c03
Fabian <fabianrk@gmail.com>
2020-03-23 12:26:20 +0200

    3 endpoints

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request
 import cv2
 
 app = Flask(__name__)
 
- 
-def gen_frames():
-    cap=  cv2.VideoCapture("rtsp://freja.hiof.no:1935/rtplive/definst/hessdalen03.stream")
-    
+
+def gen_frames(selection):
+    stream = ["rtsp://EU40.cast-tv.com:1935/23595_LIVE_Kotel_Live1/23595_LIVE_Kotel_Live1",
+              "rtsp://EU40.cast-tv.com:1935/23595_LIVE_Kotel_LIVE2/23595_LIVE_Kotel_LIVE2",
+              "rtsp://freja.hiof.no:1935/rtplive/definst/hessdalen03.stream"
+              ]
+    # cap = cv2.VideoCapture(
+    #     "rtsp://freja.hiof.no:1935/rtplive/definst/hessdalen03.stream")
+
+    cap = cv2.VideoCapture(
+        stream[selection])
     while True:
         # for cap in caps:
         # # Capture frame-by-frame
@@ -17,14 +24,27 @@ def gen_frames():
             ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
 
 
-@app.route('/video_feed', methods=["GET"])
-def video_feed():
-   
+@app.route('/video0', methods=["GET"])
+def video0():
     """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(gen_frames(),
+    return Response(gen_frames(0),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/video1', methods=["GET"])
+def video1():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen_frames(1),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/video2', methods=["GET"])
+def video2():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen_frames(2),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
@@ -33,5 +53,28 @@ def index():
     return render_template('index.html')
 
 
+# @app.route('/streaming/<selection>', methods=["GET"])
+# def streaming(selection):
+
+#     return render_template('stream.html')
+
+@app.route('/streaming/0', methods=["GET"])
+def stream0():
+
+    return render_template('stream0.html')
+
+
+@app.route('/streaming/1', methods=["GET"])
+def stream1():
+
+    return render_template('stream1.html')
+
+
+@app.route('/streaming/2', methods=["GET"])
+def stream2():
+
+    return render_template('stream2.html')
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
